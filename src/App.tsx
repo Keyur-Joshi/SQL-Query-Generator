969e0a66f603fd38c302176147212a6eab68bb7d
Keyur-JOshi <joshikeyur6697@gmail.com>
2023-04-20 14:33:48 -0400

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import CodeDisplay from "./components/CodeDisplay";
+import MessagesDisplay from "./components/MessagesDisplay";
 
-function App() {
+interface chatData {
+  role: string;
+  content: string;
+}
+
+const App = () => {
+  const [value, setValue] = useState<string>("");
+  const [chat, setChat] = useState<chatData[]>([]);
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      getQuery();
+    }
+  };
+
+  const getQuery = async () => {
+    try {
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          message: value
+        })
+      };
+      const response = await fetch(
+        "http://localhost:8000/completions",
+        options
+      );
+
+      const data = await response.json();
+      console.log(data);
+      const userMessage = {
+        role: "user",
+        content: value
+      };
+      setChat((oldChat) => [...oldChat, data, userMessage]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const clearChat = async () => {
+    setValue("");
+    setChat([]);
+  };
+
+  const filterUserMessages = chat.filter((message) => message.role === "user");
+  const latestCode = chat
+    .filter((message) => message.role === "assistant")
+    .pop();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <MessagesDisplay userMessages={filterUserMessages} />
+      <input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      <CodeDisplay text={latestCode?.content || ""} />
+      <div className="button-container">
+        <button id="get-query" onClick={getQuery}>
+          Get Query
+        </button>
+        <button id="clear-chat" onClick={clearChat}>
+          Clear Chat
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/MessagesDisplay.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import MessageDisplay from "./MessageDisplay";
+
+interface userMessage {
+  role: string;
+  content: string;
+}
+
+interface MessageDisplayProps {
+  userMessages: userMessage[];
+}
+const MessagesDisplay = ({ userMessages }: MessageDisplayProps) => {
+  return (
+    <div className="messages-display">
+      {userMessages.map((userMessage, _index) => (
+        <MessageDisplay key={_index} message={userMessage} />
+      ))}
+    </div>
+  );
+};
+
+export default MessagesDisplay;
